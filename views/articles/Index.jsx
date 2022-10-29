4ff915b125600ad8769c2e420ea8faa5b52031b3
yevagav <yevagavrilov@gmail.com>
2022-10-28 22:04:20 -0700

    added styling to new page

--- views/articles/Index.jsx
+++ views/articles/Index.jsx
@@ -5,27 +5,32 @@ class Index extends React.Component {
   render () {
     const { articles } = this.props
     return (
-      <Default title ='TRAVEL BLOG'>
+      <Default>
+        <div className='container'>
         <ul>
           {
             articles.map((article) => {
               const { title, post, image, _id } = article
               return (
-                <li key={_id}>
-                  <a href={`/articles/${_id}`}>
+                <div className='flexItems'>
+                <li className="li" key={_id}>
+                  <a className="a" href={`/articles/${_id}`}>
                     {title}
-                  </a> {post}
+                  </a> 
                   <br />
                   {image ? <img src={image} /> : ' '}
                   <form method='POST' action={`/articles/${_id}?_method=DELETE`}>
-                    <input type='submit' value={`Delete ${title}`} />
+                    <input className='delete' type='submit' value={`Delete Post`} />
 
                   </form>
                 </li>
+                </div>
+                
               )
             })
           }
         </ul>
+        </div>
       </Default>
     )
   }
--- views/articles/New.jsx
+++ views/articles/New.jsx
@@ -4,13 +4,17 @@ const Default = require('../layouts/Default.jsx')
 class New extends React.Component {
   render () {
     return (
-      <Default title='NEW ITINERARY'>
+      <Default title='NEW BLOG POST'>
         <h2>NEW BLOG POST</h2>
         <form method='POST' action='/articles'>
+          <div className='newBox'>
+            <div className='newCont'>
           Title: <input type='text' name='title' placeholder='Title Of Blog Post' /><br />
-          Post: <input type='text' name='post' placeholder='Type Your Post Here' /><br />
+          Post: <input className='newInput' type='text' name='post' placeholder='Type Your Post Here' /><br />
           Image: <input type='text' name='image' placeholder='URL of your image' /><br />
           <input type='submit' value='Submit Post' />
+          </div>
+          </div>
         </form>
       </Default>
     )
